8fae7e92a70cc430037cba67012cf6314951f81c
Adityashah2003 <adigshah14@gmail.com>
2022-07-11 15:24:07 +0530

    second code

---
+++ p-c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (){
+    int num=0,p=0,c=0,i=0,count=0;
+    do{
+        scanf("%d",&num);
+        for(int i=2 ; i < num/2 ; i++) {
+        if(num%i == 0){
+            p++;
+            count = 1;
+            break;
+            }
+        }
+        if(count == 0) {
+          c++;
+        }  
+    }while(num != -1);
+    printf("%d\n Prime numbers:",p);
+    printf("%d\n Composite numbers:",c);
+    return 0;
+}
