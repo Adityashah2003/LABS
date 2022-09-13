ca5a8f4de9880702492bdde4420281276c0c889f
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2022-09-14 00:03:08 +0530

    Create add_3_1.java

---
+++ SEM-3/OOP/Lab 2&3/add_3_1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main{
+public static void main(String args[]){
+int a[] = { 1, 2,3,4}; 
+for (int w = 0; w < 4; w++) 
+{
+    for (int x = 0; x < 4; x++) 
+     {
+        for (int y = 0; y <4; y++) 
+        {
+          for (int z = 0; z < 4; z++) 
+          {
+             if (w!=x && x != y && y != z && z != x) 
+             { 
+             System.out.println(a[w] + "" + a[x] + "" + a[y] + "" + a[z]); 
+             } 
+          }
+        }
+     }
+  }
+}
+}
