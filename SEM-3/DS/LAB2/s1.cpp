f22538f513f567b1cf9313047b6a0eae8b4f89a7
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2022-09-16 17:55:25 +0530

    Update s1.cpp

--- SEM-3/DS/LAB2/s1.cpp
+++ SEM-3/DS/LAB2/s1.cpp
@@ -7,7 +7,7 @@ int main() {
     char str[100],str1[100],str2[100];
     int i,j=0;
     gets(str);
-    for(i=0; str[i]!='\0'; i++){ }
+    for(i=0; str[i]!='\0'; i++)
         cout << "the length of the string is " << i;
 return 0;
 }
