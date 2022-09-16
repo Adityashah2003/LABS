f784d5e78f15794c966d2cfbf30f5c0b734de232
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2022-09-16 12:54:00 +0530

    Update multiple_stacks.cpp

--- SEM-3/DS/LAB3/multiple_stacks.cpp
+++ SEM-3/DS/LAB3/multiple_stacks.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+#define Maxsize 100
+
 class mstack{
     int top[20],bottom[20];
     int ns,maxsize=0;
@@ -13,22 +19,23 @@ public:
         ns=n;
         maxsize=ms;
     }
-    void push(int.int);
+    void push(int, int);
     int pop(int);
-    void pop(int);
     void display(int);
 
-}
-void mstack::push(push int i,int ele){
+};
+void mstack::push(int i,int ele){
     if(top[i]==bottom[i+1]||top[i]==maxsize-1)
     {
         cout<<"ith stack is full";
-        return 0;
+        return;
     }
-++top[i];
-s[top[i]]=ele;
+    else{
+        ++top[i];
+        s[top[i]]=ele;
+    }
 }
-int mystack:: pop(int i){
+int mstack:: pop(int i){
     int ele;
     if (top[i]==bottom[i]){
         cout<<i<<"th stack is empty";
@@ -40,7 +47,7 @@ int mystack:: pop(int i){
         return(ele);
     }
 }
-void mstack::dsiplay(int i){
+void mstack::display(int i){
     int index = top[i];
     if(top[i]==bottom[i])
         cout<<i<<"th stack is empty";
@@ -49,4 +56,17 @@ void mstack::dsiplay(int i){
               cout<<s[index];
               index = index-1;
     }
+}
+}
+
+int main()
+{
+    mstack s;
+    int ele;
+    s.push(10,15);
+    s.push(20,25);
+    s.push(30,35);
+    s.pop(15);
+    s.display(10);
+return 0;
 }
