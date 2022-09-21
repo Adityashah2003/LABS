92fe7f57efd5cb3fffd4e6a6f2da7cd5b121ad63
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2022-09-21 10:03:48 +0530

    Update TimeClass.java

--- SEM-3/OOP/Lab5/TimeClass.java
+++ SEM-3/OOP/Lab5/TimeClass.java
@@ -1,49 +1,44 @@
+import java.util.*;
 
-import java.util.Scanner;
+class sec{
+    int hours,minutes,seconds=0;
+    sec(){
+        hours = minutes = seconds =0;
+    }                               //default constructor
 
-import javax.management.loading.MLet;
-class Time {
-  int hour, min, sec;
+    sec(int h,int m,int s){
+        hours =h;
+        minutes=m;
+        seconds =s;
+    }                                //parameterised constructor
+    void display(){
+        System.out.println("HH:MM:SS is: " + hours+":"+minutes+":"+ seconds);
+    }
+    void add(sec a,sec b){
+        seconds = (a.seconds + b.seconds)%60;
+        minutes = (a.minutes + b.minutes)%60 + (a.seconds+b.seconds)/60;
+        hours = (a.hours+b.hours) %24 + (a.minutes + b.minutes)/60;
+    }
+};
 
-  Time() { hour = min = sec = 0;}
-  Time(int h, int m, int s)
-  {
-    hour = h;
-    min = m;
-    sec = s;
-  }
-
-  void printTime()
-  { System.out.println( hour +":"+ min +":"+ sec);}
-  
-  void AddTime(Time a, Time b)
-  {
-    sec = (a.sec + b.sec)%60;
-    min = (a.min + b.min)%60 + (a.sec+b.sec)/60;
-    hour = (a.hour+b.hour) %24 + (a.min + b.min)/60;
+class Time{
+    public static void main(String args[]) {
+        sec obj1,obj2,sum;
+        obj1 = new sec();
+        obj2 = new sec(05,45,30);
+        sum = new sec();
+        
+        
+        System.out.print("Object 1 default initialised to value \t");
+        obj1.display();
+        System.out.print("Object 2 manually initialised to value \t");
+        obj2.display();
     
-  }
-}
-
-class TimePrg{
-  public static void main(String args[])
-  {
-    Time obj1, obj2, Sum;
-    obj1 = new Time();
-    obj2 = new Time(5, 45, 30);
-    Sum = new Time();
-
-    System.out.print("Object 1 default initialised to value \t");
-    obj1.printTime();
-    System.out.print("Object 2 manually initialised to value \t");
-    obj2.printTime();
-
-    System.out.print("Object 1 = Obj2+Obj2 \t\t \t");
-    obj1.AddTime(obj2, obj2);
-    obj1.printTime();
-    System.out.print("Sum = Obj1 + Obj2 \t\t\t");
-    Sum.AddTime(obj1, obj2);
-    Sum.printTime();
-
-  }
+        /*System.out.print("Object 1 = Obj2+Obj2 \t\t \t");
+        obj1.add(obj2, obj2);
+        obj1.display();*/
+        System.out.print("Sum = Obj1 + Obj2 \t\t\t");
+        sum.add(obj1, obj2);
+        sum.display();
+    }
 }
