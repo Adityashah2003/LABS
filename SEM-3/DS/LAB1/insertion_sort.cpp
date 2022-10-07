423e013593f714776943d8cb024d66f632ae8da9
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2022-10-07 23:48:17 +0530

    Update insertion_sort.cpp

--- SEM-3/DS/LAB1/insertion_sort.cpp
+++ SEM-3/DS/LAB1/insertion_sort.cpp
@@ -7,10 +7,8 @@ void insertionSort(int arr[], int n)
 	for (i = 0; i < n; i++)
 	{
 	key = arr[i];
-		for(j=i;j >= 0 && arr[j] > key;j--){
-		arr[j + 1] = arr[j];
-		j = j - 1;
-		}
+		for(j=i;j >= 0 && arr[j] > key;j--)
+			arr[j + 1] = arr[j];
 	arr[j + 1] = key;
 	}
 }
