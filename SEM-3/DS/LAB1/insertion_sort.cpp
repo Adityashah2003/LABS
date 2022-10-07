63eefa5f96528ab42a328aa434d17b0e01093279
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2022-10-07 23:47:28 +0530

    Update insertion_sort.cpp

--- SEM-3/DS/LAB1/insertion_sort.cpp
+++ SEM-3/DS/LAB1/insertion_sort.cpp
@@ -4,16 +4,14 @@ using namespace std;
 void insertionSort(int arr[], int n)
 {
 	int i, key, j;
-	for (i = 1; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		key = arr[i];
-		j = i - 1;
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
+	key = arr[i];
+		for(j=i;j >= 0 && arr[j] > key;j--){
+		arr[j + 1] = arr[j];
+		j = j - 1;
 		}
-		arr[j + 1] = key;
+	arr[j + 1] = key;
 	}
 }
 void printArray(int arr[], int n)
