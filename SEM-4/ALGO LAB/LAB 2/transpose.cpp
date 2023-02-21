f61a5cc3b293f915f98272d23300da86f87477c4
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2023-02-21 22:30:05 +0530

    Update transpose.cpp

--- SEM-4/ALGO LAB/LAB 2/transpose.cpp
+++ SEM-4/ALGO LAB/LAB 2/transpose.cpp
@@ -4,14 +4,17 @@ using namespace std;
 queue <int> q;
 int vertices, edges;
 int adj[10][10];
+int counter=0;
 
 void addEdge(int x,int y){
     adj[x][y] = 1;
+    counter++;
 }
 
 int main(){
 
     int i, j,m=0,n=0;
+    counter+=4;
     cout << "Enter Number of Vertices : ";
     cin >> vertices;
     cout << "Enter Number of Edges : ";
@@ -22,6 +25,7 @@ int main(){
         cin>>m;
         cin>>n; 
         addEdge(m,n);
+        counter++;
     }
 
     cout << "\nThe Adjacency Matrix is : \n\n";
