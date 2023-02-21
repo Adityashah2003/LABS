4958a2585abecda930767d2ca56c4ae1b2a3cfb8
Aditya <84448014+Adityashah2003@users.noreply.github.com>
2023-02-21 21:04:38 +0530

    Update insertion_sort.cpp

--- SEM-4/ALGO LAB/LAB 1/insertion_sort.cpp
+++ SEM-4/ALGO LAB/LAB 1/insertion_sort.cpp
@@ -1,18 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int counter=0;
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
+    counter++;
     for (i = 1; i < n; i++){
+        counter++;
         key = arr[i];
         j = i - 1;
+        counter++;
         while (j >= 0 && arr[j] > key){
+            counter++;
             swap(arr[j + 1],arr[j]);
             j = j - 1;
+            counter++;
+            counter++;
         }
+        counter++;
+        counter++;
         arr[j + 1] = key;
     }
+    counter++;
 }
 int main(){
     int n=0;
